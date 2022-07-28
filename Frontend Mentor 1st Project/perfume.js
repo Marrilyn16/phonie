d56098d5606a5f10849f8f94480ee22176a51f1d
Marrilyn <marrilyno16@gmail.com>
2022-07-28 15:07:27 +0100

    first commit

---
+++ Frontend Mentor 1st Project/perfume.js
@@ -0,0 +1,9 @@
+// const button = document.querySelector('#submit-btn');
+
+// Mouse Enter Event
+// button.addEventListener('mouseenter', () => {
+//     alert("You've entered the button!");
+// });
+function displayConfirmation() {
+    alert("Are you sure you want to add selected item to cart")
+}
---
+++ JavaScript/Week1/loops/index.js
@@ -0,0 +1,49 @@
+// //while loop
+// let i = 0;
+// while (i < 10) { // shows 0, then 1, then 2
+//     console.log(i);
+//     i++;
+// }
+numbers=[1,2,3,4,5]
+function sumOfNumbers(numbers) {
+    let sum=0;
+    for=
+
+}
+
+
+// //do while
+// let j = 5;
+// do {
+//     console.log(j);
+//     j++;
+// } while (j < 3)
+
+
+// //for loop
+// for (let i = 0; i <=5; i++) {
+//     console.log("i is now", i )
+// }
+
+
+// //for..of
+// const iterable = [10, 20, 30];
+
+// for (const value of iterable) {
+//     console.log(value)
+// }
+
+// const firstname = "Johnson"
+
+// for (const Char of firstname) {
+//     console.log("character is:", Char);
+// }
+
+
+// //for ... in
+
+// const object = { name: "John", age: 20 };
+
+// for (const key in object) {
+//     console.log(key)
+// }
